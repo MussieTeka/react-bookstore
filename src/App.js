6f46dd752d9e42ea306347180bde2e6a39192969
Mussie Kahsay <mussiekahasay12@gmail.com>
2023-05-11 00:40:09 +0300

    Modify App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import Navigation from './components/Navigation';
 import Books from './pages/Books';
 import Categories from './pages/Categories';
 
 function App() {
   return (
-    <Router>
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<Books />} />
-        <Route path="/categories" element={<Categories />} />
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Books />} />
+          <Route path="/categories" element={<Categories />} />
+        </Routes>
+      </Router>
+    </Provider>
   );
 }
 
