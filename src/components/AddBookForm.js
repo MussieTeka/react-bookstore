b3e641323a193db8023fd1471534e888ede17358
Mussie Kahsay <mussiekahasay12@gmail.com>
2023-05-11 23:49:01 +0300

    Modify addBookForm.js

--- src/components/AddBookForm.js
+++ src/components/AddBookForm.js
@@ -1,20 +1,25 @@
 import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/booksSlice';
 
-const AddBookForm = ({ handleAddBook }) => {
+const AddBookForm = () => {
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
+  const [category, setCategory] = useState('');
+  const dispatch = useDispatch();
 
   const handleSubmit = (event) => {
     event.preventDefault();
     const newBook = {
-      id: Date.now(),
+      item_id: `item${Date.now()}`,
       title,
       author,
+      category,
     };
-    handleAddBook(newBook);
+    dispatch(addBook(newBook));
     setTitle('');
     setAuthor('');
+    setCategory('');
   };
 
   return (
@@ -29,14 +34,22 @@ const AddBookForm = ({ handleAddBook }) => {
           Author:
           <input type="text" value={author} onChange={(event) => setAuthor(event.target.value)} required />
         </div>
+        <div>
+          Category:
+          <select value={category} onChange={(event) => setCategory(event.target.value)} required>
+            <option value="">Select a category</option>
+            <option value="Fiction">Fiction</option>
+            <option value="Non-fiction">Non-fiction</option>
+            <option value="Science fiction">Science fiction</option>
+            <option value="Mystery">Mystery</option>
+            <option value="Thriller">Thriller</option>
+            <option value="Romance">Romance</option>
+          </select>
+        </div>
         <button type="submit">Add Book</button>
       </form>
     </div>
   );
-};
-
-AddBookForm.propTypes = {
-  handleAddBook: PropTypes.func.isRequired,
 };
 
 export default AddBookForm;
