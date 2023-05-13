5f6c73ed97e0c14d65d218da6fe981de77bcf12e
Mussie Kahsay <mussiekahasay12@gmail.com>
2023-05-13 21:59:08 +0300

    Modify BookList.js

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { removeBook } from '../redux/books/booksSlice';
+import '../styles/components/BookList.css';
 
 const BooksList = ({ books }) => {
   const dispatch = useDispatch();
@@ -13,26 +14,37 @@ const BooksList = ({ books }) => {
   return (
     <div>
       {books.length > 0 ? (
-        <ul>
+        <ul className="book-list">
           {books.map((book) => (
-            <li key={book.item_id}>
-              <div>
-                <strong>Title:</strong>
-                {' '}
-                {book.title}
+            <li key={book.id} className="book-list-item">
+              <div className="book-details">
+                <div className="book-category">
+                  {book.category}
+                </div>
+                <div className="book-title">
+                  {book.title}
+                </div>
+                <div className="book-author">
+                  {book.author}
+                </div>
+                <div className="book-actions">
+                  <button type="button" className="button-actions">Comments</button>
+                  <button type="button" className="button-actions" onClick={() => handleDeleteBook(book)}>Delete</button>
+                  <button type="button" className="button-actions">Edit</button>
+
+                </div>
               </div>
-              <div>
-                <strong>Author:</strong>
-                {' '}
-                {book.author}
+              <div className="book-progress">
+                <span className="progress-circle" />
+                <div className="percent">
+                  <span className="percent-number">64%</span>
+                  <span className="percent-complete">Completed</span>
+                </div>
               </div>
-              <div>
-                <strong>Category:</strong>
-                {' '}
-                {book.category}
-              </div>
-              <div>
-                <button type="button" onClick={() => handleDeleteBook(book)}>Delete</button>
+              <div className="book-chapter">
+                <span className="current-chapter">CUREENT CHAPTER</span>
+                <span className="current-lesson">Chapter 17</span>
+                <button type="button">UPDATE PROGRESS</button>
               </div>
             </li>
           ))}
