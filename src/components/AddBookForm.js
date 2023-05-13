50c8f824ebaa235d67d9c8a237ba588dcccd812c
Mussie Kahsay <mussiekahasay12@gmail.com>
2023-05-13 21:58:41 +0300

    Modify AddBookForm.js

--- src/components/AddBookForm.js
+++ src/components/AddBookForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { addBook, fetchBooks } from '../redux/books/booksSlice';
+import '../styles/components/AddBookForm.css';
 
 const AddBookForm = () => {
   const [title, setTitle] = useState('');
@@ -24,21 +25,19 @@ const AddBookForm = () => {
   };
 
   return (
-    <div>
+    <div className="add-book">
+      <hr className="horizontal-line" />
       <h2>Add Book</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          Title:
-          <input type="text" value={title} onChange={(event) => setTitle(event.target.value)} required />
+      <form onSubmit={handleSubmit} className="add-book-form">
+        <div className="title">
+          <input type="text" placeholder="Book title" value={title} onChange={(event) => setTitle(event.target.value)} required />
         </div>
-        <div>
-          Author:
-          <input type="text" value={author} onChange={(event) => setAuthor(event.target.value)} required />
+        <div className="author">
+          <input type="text" placeholder="Book author" className="author" value={author} onChange={(event) => setAuthor(event.target.value)} required />
         </div>
-        <div>
-          Category:
+        <div className="category">
           <select value={category} onChange={(event) => setCategory(event.target.value)} required>
-            <option value="">Select a category</option>
+            <option value="">Category</option>
             <option value="Fiction">Fiction</option>
             <option value="Non-fiction">Non-fiction</option>
             <option value="Science fiction">Science fiction</option>
